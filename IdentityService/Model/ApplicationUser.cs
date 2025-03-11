abb113bdbbdf5fef93620ac8b236b6d82122c5d2
Gabriel Prata <gabrielsprata@hotmail.com>
2025-03-11 16:55:30 -0300

    [wip] Desenvolvimento do IdentityService

---
+++ IdentityService/Model/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        private int id {  get; set; }
+        private string Nome {  get; set; }
+        private string Sobrenome { get; set; } 
+        private string Email {  get; set; }
+
+    }
+}
---
+++ IdentityService/Models/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityService.Models.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string User = "User";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes() =>
+            new List<ApiScope>
+            {
+                new ApiScope("schoolMatch", "AccountService"),
+                new ApiScope(name: "read", "Read Data"),
+                new ApiScope(name: "write", "Write Data"),
+                new ApiScope(name: "delete", "Delete Data"),
+            };
+
+        public static IEnumerable<Client> GetClients(IConfiguration configuration)
+        {
+            var clientSecret = configuration["IdentitySettings:ClientSecret"]; // Obtém do appsettings.json
+
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret(clientSecret.Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read", "write", "profile"}
+                }
+            };
+        }
+    };
+}
